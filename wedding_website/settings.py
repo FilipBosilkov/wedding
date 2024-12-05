9b0b54b1a2f636ae3820e056fa01fe8bd6c8d6ec
FilipBosilkov <filip_b01@outlook.com>
2024-12-05 22:33:04 +0100

    adding things

--- wedding_website/settings.py
+++ wedding_website/settings.py
@@ -12,8 +12,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +29,9 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['filipandhailee.com']
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://filipandhailee.com',
+]
 
 # Application definition
 
@@ -41,8 +47,9 @@ INSTALLED_APPS = [
     'corsheaders'
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
-
+CORS_ALLOWED_ORIGINS = [
+    "https://filipandhailee.com",
+]
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
