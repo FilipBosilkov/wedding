1022a4e4aef76f36a6ef516a6b32e0b5550d0d1a
FilipBosilkov <filip_b01@outlook.com>
2024-12-17 00:55:16 +0100

    captcha 3

--- homepage/urls.py
+++ homepage/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, re_path
 from django.views.generic import TemplateView
-from . import views
 
 urlpatterns = [
     # API routes
